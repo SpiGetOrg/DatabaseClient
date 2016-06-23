30bca8c80d5d6e22287d37316cc7f8d47574df76
inventivetalent <inventivetalent@inventivegames.de>
2016-06-23 14:55:38 +0200

    add codec to properly handle DBRefs

--- src/main/java/org/spiget/database/DatabaseParser.java
+++ src/main/java/org/spiget/database/DatabaseParser.java
@@ -3,17 +3,25 @@ package org.spiget.database;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.mongodb.MongoClient;
 import org.bson.Document;
+import org.bson.codecs.BsonTypeClassMap;
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
 
 public class DatabaseParser {
 
+	final static CodecRegistry CODEC_REGISTRY = CodecRegistries.fromRegistries(MongoClient.getDefaultCodecRegistry());
+	final static DocumentCodec CODEC          = new DocumentCodec(CODEC_REGISTRY, new BsonTypeClassMap());
+
 	public static JsonObject toJson(Document document) {
 		if (document == null) { return null; }
-		return new JsonParser().parse(document.toJson()).getAsJsonObject();
+		return new JsonParser().parse(document.toJson(CODEC)).getAsJsonObject();
 	}
 
 	public static Document toDocument(JsonElement jsonObject) {
-		return Document.parse(jsonObject.toString());
+		return Document.parse(jsonObject.toString(), CODEC);
 	}
 
 }
